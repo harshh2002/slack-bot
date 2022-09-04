cfd63c6bfdebe45a72dbc63557299d9c43d6d5c2
Harsh Pratap Singh <harsh@Harshs-MacBook-Pro.local>
2022-09-04 13:14:05 +0530

    auth

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import slack
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-SLACK_TOKEN="xoxb-4032863284611-4030037183557-ICdmYASAzX9O9qo3zP036jhF"
-
+SLACK_TOKEN=os.getenv("SLACK_TOKEN")
+print(SLACK_TOKEN)
 client = slack.WebClient(token=SLACK_TOKEN)
 client.chat_postMessage(channel='#slack-bot',text='Hello world')
